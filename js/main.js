2692f2f11f929416b9841baba1e94c884580b524
youbs <hubert.moutot@gmail.com>
2015-03-20 21:24:54 +0100

    added form confirmation message

--- js/main.js
+++ js/main.js
@@ -24,8 +24,29 @@ jQuery(document).ready(function($) {
 
 	set_skill_percent();
 
+
+	if(getQueryParam('thanks') == '') {
+		$('.form-thanks').show();
+	}
+
 });//end document ready
 
+
+function getQueryParam(str)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == str){
+               	if(pair[1]) {
+               		return pair[1];
+               	}
+               return '';
+       		}
+       }
+       return(false);
+}
 
 /* set skill percent
 =========================================== */
